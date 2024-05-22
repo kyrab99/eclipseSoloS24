e44ba7d6a4c74c0b66c5042038a03ef0ae89af41
zbenj <zbenj@172.16.63.234>
2024-05-22 12:57:46 -0400

    making changes from project one

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Howdy Github Project");
-		System.out.println("Programmer 2 did this");
+		System.out.println("\"ProgrammerOne did this");
 	}
 
 }
